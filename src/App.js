9bc3c1e477e1c49e601467b18a66c6529c62a8b4
florella-yannis <florellayannis@gmail.com>
2023-04-27 13:34:27 +0200

    Nav + heroBanner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Nav from './components/Nav.js';
+import HeroBanner from './components/HeroBanner.js';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Nav />
         <h1>hello</h1>
+        <HeroBanner />
       </header>
     </div>
   );
